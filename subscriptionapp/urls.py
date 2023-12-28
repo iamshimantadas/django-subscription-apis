b2821f91fb77cd78235bac4d82d58d5899693675
SHIMANTA DAS <iamshimantadas@gmail.com>
2023-12-28 15:14:17 +0530

    path modified

--- subscriptionapp/urls.py
+++ subscriptionapp/urls.py
@@ -5,7 +5,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 from home.urls import *
 
 urlpatterns = [
-    path('',include('home.urls')),
+    path('home/',include('home.urls')),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/doc/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
