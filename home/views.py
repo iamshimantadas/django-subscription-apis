f0521e25ff9cb0970a770bc20646e27e26d874e6
SHIMANTA DAS <iamshimantadas@gmail.com>
2023-12-28 14:56:59 +0530

    home page api added

--- home/views.py
+++ home/views.py
@@ -10,7 +10,37 @@ from core.models import Carousel
 
 class HomeView(APIView):
     serializer_class = HomeSerializer
+    
     def get(self, request):
         home_obj  = Carousel.objects.all()
         serializer = self.serializer_class(home_obj, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+        try:
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response({"message":"new home page data added"},status=status.HTTP_201_CREATED)
+            else:
+                return Response({"message":"error occured!"},status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            print(e)
+            return Response({"message":"error occured!"})
+
+    def put(self, request, pk=None, format=None):
+        userid = pk
+        carousel_obj = Carousel.objects.get(id=userid)
+        try:
+            serializer = self.serializer_class(carousel_obj,data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response({"message":"home page info updated!"},status=status.HTTP_200_OK)
+            else:
+                return Response({"message":"info not updated! try again! "},status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            print(e)
+        return Response({"message":"error occured!"},status=status.HTTP_400_BAD_REQUEST)    
+    
+    def destroy(self, request, pk=None, format=None):
+        userid = pk
--- readme.txt
+++ readme.txt
@@ -1,5 +1,9 @@
 This is a django project made for subscription management!
 
+** APS details **
+1. http://localhost:8000/ => show details about home page - carousels and description
+2. 
+
 ** note: django admin **
 email[username]: admin@admin.com
 password: admin
