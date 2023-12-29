8bf489aea04f3e1ae84c3c89b94408e8dcce1d1d
SHIMANTA DAS <iamshimantadas@gmail.com>
2023-12-29 10:50:29 +0530

    update method now allow updated

--- contact/views.py
+++ contact/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from core.models import ContactUs
 from .serializers import *
+from rest_framework.response import Response
 
 class ContactView(ModelViewSet):
     queryset = ContactUs.objects.all()
@@ -10,3 +11,8 @@ class ContactView(ModelViewSet):
     permission_classes = []
     authentication_classes = []
 
+    def update(self, request, *args, **kwargs):
+        return Response({"message":"update method not allowed"})
+
+    def partial_update(self, request, *args, **kwargs):
+        return Response({"message":"partial update method not allowed"})
