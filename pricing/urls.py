5255eceabcb052f31f12a92635c968b2790559ca
SHIMANTA DAS <iamshimantadas@gmail.com>
2024-01-08 19:37:33 +0530

    all product details and product info by ID api route added

--- pricing/urls.py
+++ pricing/urls.py
@@ -1,17 +1,9 @@
 from django.urls import path, include
-from rest_framework.routers import SimpleRouter
+
 from .views import *
-
-# router = SimpleRouter()
-# router.register("",PriceView)
-
-# pricing_detail_router = SimpleRouter()
-# pricing_detail_router.register("",Pricing_detail_view)
 
 urlpatterns = [
     path("plans",PlanView.as_view()),
     path("plans/<str:pk>/",PlanView.as_view()),
-    # path("pricing-detail/",include(pricing_detail_router.urls)),
 ]
 
-# urlpatterns = urlpatterns + router.urls
--- pricing/views.py
+++ pricing/views.py
@@ -13,42 +13,20 @@ from django.conf import settings
 import stripe
 
 
-# class PriceView(ModelViewSet):
-#     queryset = Pricing.objects.all()
-#     serializer_class = PricingSerializer
-#     permission_classes = []
-#     authentication_classes = []
-
-
 @extend_schema(tags=['plans'])
 class PlanView(APIView):
-    def get_object(self, pk):
-        prodid = pk
-        if prodid:
-            try:
-                info = stripe.Product.retrieve("prodid")
-                # info = prodid
-                return Response({"product":info})
-            except Exception as e:
-                print(e)
-                return Response({"status":"error"})
-        else:
-            return Response({"message":"enter product ID correctly! ex: prod_PKyAFU3gwXFR"})
-        
-    def get(self, request):
+    
+    def get(self, request, pk=None):
         try:
             stripe.api_key = settings.STRIPE_SECRET_KEY
-            products = stripe.Product.list()
-            return Response({"product":products})
-        except Exception as e:
+
+            if pk:  # If a product ID is provided, fetch information about that specific product
+                product_info = stripe.Product.retrieve(pk)
+                return Response({"product": product_info})
+            else:  # If no product ID is provided, list all products
+                products = stripe.Product.list()
+                return Response({"products": products})
+        except stripe.error.StripeError as e:
             print(e)
-            return Response({"status":"error"})
+            return Response({"status": "error"}, status=status.HTTP_400_BAD_REQUEST)
 
-
-
-# @extend_schema(tags=['pricing details'])
-# class Pricing_detail_view(ModelViewSet):
-#     queryset = Pricing_detail.objects.all()
-#     serializer_class = PricingDetailSerializer
-#     authentication_classes = []
-#     permission_classes = []
