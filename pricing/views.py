97f2725e50ab88d9fc164161779e26cf85e4cbdc
SHIMANTA DAS <iamshimantadas@gmail.com>
2024-01-09 12:02:03 +0530

    minor

--- pricing/views.py
+++ pricing/views.py
@@ -13,20 +13,28 @@ from django.conf import settings
 import stripe
 
 
-@extend_schema(tags=['plans'])
+@extend_schema(tags=["plans"])
 class PlanView(APIView):
-    
     def get(self, request, pk=None):
-        try:
-            stripe.api_key = settings.STRIPE_SECRET_KEY
-
-            if pk:  # If a product ID is provided, fetch information about that specific product
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        if pk:
+            try:
                 product_info = stripe.Product.retrieve(pk)
                 return Response({"product": product_info})
-            else:  # If no product ID is provided, list all products
+            except Exception as e:
+                print(e)
+                return Response({"status":"error"})
+        else:
+            try:
                 products = stripe.Product.list()
                 return Response({"products": products})
-        except stripe.error.StripeError as e:
-            print(e)
-            return Response({"status": "error"}, status=status.HTTP_400_BAD_REQUEST)
+            except Exception as e:
+                print(e)
+                return Response({"status":"error"})    
+        # try:
+            
+        # except stripe.error.StripeError as e:
+        #     print(e)
+        #     return Response({"status": "error"}, status=status.HTTP_400_BAD_REQUEST)
 
+    
